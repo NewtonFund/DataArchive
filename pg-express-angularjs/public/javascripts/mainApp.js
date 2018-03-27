1df593fb8e67a93ef374542bde2b8132afa43fa1
cylammarco <cylammarco@gmail.com>
2018-03-27 10:48:44 +0100

    Dynamic ng-table

---
+++ pg-express-angularjs/public/javascripts/mainApp.js
@@ -0,0 +1,193 @@
+var mainApp = angular.module("mainApp", ["ngTable"]);
+
+mainApp.controller('psqlController', function($scope, $http) {
+
+  $scope.query = function()
+  {
+      $http.get("/query_fixed")
+      .then(function(response) {
+          //First function handles success
+          $scope.data = response.data;
+          //console.log(response.data);
+      }, function(response) {
+          //Second function handles error
+          console.log("Something went wrong");
+      });
+  };
+
+});
+
+
+mainApp.controller('psqlController2', function($scope, $http) {
+
+  $scope.query_obsnum = function(minval)
+  {
+      $http.post("/query_obsnum", {minval})
+      .then(function(response) {
+          //First function handles success
+          $scope.data = response.data;
+          //console.log(response.data);
+      }, function(response) {
+          //Second function handles error
+          console.log("Something went wrong");
+      });
+  };
+
+});
+
+
+mainApp.controller('queryFormController', function($scope, $http, $window, NgTableParams) {
+
+  $scope.checkboxModel = {
+    position: true,
+    mjd: false,
+    exptime: false,
+    azimuth: false,
+    altitude: false,
+    seeing: false,
+    airmass: false,
+    tagid: false,
+    userid: false,
+    propid: false,
+    groupid: false,
+    obsid: false,
+    limit: true
+  };
+
+  $scope.queryModel = {
+    RA: 101.287,
+    DEC: -16.716,
+    RADIUS: 60.0,
+    MJD_min: 0,
+    MJD_max: 58000,
+    EXPTIME_min: 0.,
+    EXPTIME_max: 3600.,
+    AZI_min: 0.0,
+    AZI_max: 360.0,
+    ALT_min: 0.0,
+    ALT_max: 90.0,
+    SEEING_min: 0.0,
+    SEEING_max: 5.0,
+    AIRMASS_min: 0.0,
+    AIRMASS_max: 5.0,
+    TAGID: 0,
+    USERID: 0,
+    PROPID: 0,
+    GROUPID: 0,
+    OBSID: 0,
+    LIMIT: 1000
+  };
+
+  $scope.send_query = function(queryModel, checkboxModel) {
+      $http.post("/query_full", {queryModel, checkboxModel})
+      .then(function(response) {
+          //First function handles success
+          var rows = response.data.rows;
+
+          //console.log(response.data.fields);
+          var colNames = Object.values(response.data.fields);
+          
+          $scope.columnNames = colNames.map(function (field, idx) {
+            var filter = {};
+            filter[field.name] = 'text';
+            return {
+              title: field.name,
+              sortable: field.name,
+              filter: filter,
+              show: true,
+              field: field.name
+            };
+          });
+
+          //console.log($scope.columnNames);
+          $scope.pgOutput = new NgTableParams({
+            page: 1,
+            count: 10
+          },
+            {
+            dataset: rows,
+            total: rows.length
+          });
+          
+          $scope.allRows = [];
+          for (var i=rows.length-1; i>=0; i--) {
+            //console.log(rows[i]);
+            $scope.allRows.push(rows[i].__obsnum);
+          }
+          console.log($scope.allRows);
+          //$scope.allRows = rows.__obsnum;
+
+      }, function(response) {
+          //Second function handles error
+          console.log("Something went wrong");
+      });
+  }
+
+  // Empty array to populate selected items
+  $scope.selectedRows = [];
+
+  // Add or remove item to array
+  $scope.select_data = function(item) {
+    // assume data does not exist
+    var exist = false;
+    // remove data if it exists
+    for (var i=$scope.selectedRows.length-1; i>=0; i--) {
+      if ($scope.selectedRows[i] === item) {
+        $scope.selectedRows.splice(i, 1);
+        // data exists
+        exist = true;
+      }
+    }
+    // if data is not in the array, append to the array
+    if (exist == false) {
+      $scope.selectedRows.push(item);
+    }
+    console.log($scope.selectedRows);
+  }
+
+  // pass the data to here >>
+  $scope.getAllSelectedRows = function()
+  {
+    if ($scope.selectedRows.length == 0) {
+      alert('No item is selected')
+    } else {
+      $http.post("/get_files", $scope.selectedRows)
+      .then(function(response)
+      {
+        console.log(response.data);
+        $scope.downloadLink = response.data;
+        $scope.downloadLinkReady = true;
+        //$window.open(response.data);
+      }, function(response)
+      {
+        console.log("Something went wrong");
+      });
+    }
+  }
+
+  $scope.getAllRows = function()
+  {
+    if ($scope.allRows.length == 0) {
+      alert('No item is selected')
+    } else {
+      $http.post("/get_files", allRows)
+      .then(function(response)
+      {
+        console.log(response.data);
+        $scope.downloadLink = response.data;
+        $scope.downloadLinkReady = true;
+        //$window.open(response.data);
+      }, function(response)
+      {
+        console.log("Something went wrong");
+      });
+    }
+  }
+
+
+
+//  $scope.select_data = function(filename) {
+//    alert('pretend this is the filepath: ' + filename);
+//  }
+
+});
--- pg-express-angularjs/routes/index.js
+++ pg-express-angularjs/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+//var childProcess = require('child_process');
+var fs = require('fs');
 const { Pool } = require('pg')
 
 const pool = new Pool({
@@ -18,7 +20,7 @@ router.get('/', function(req, res, next)
   //res.render('index', { title: 'Express' });
   //res.send("Hello");
   res.sendFile(path.join(__dirname, '../', 'index.html'));
-    
+  
 });
 
 router.get('/query_fixed', function(req, res, next) 
@@ -37,7 +39,7 @@ router.get('/query_fixed', function(req, res, next)
 
 router.post('/query_obsnum', function(req, res, next) 
 {
-  console.log(req.body);
+  //console.log(req.body);
   var min = req.body.minval;
   pool.query('SELECT __obsnum, __filename, Date, ra_degree, dec_degree from allkeys WHERE __obsnum > ' + min + ' limit 10', (err, result) => {
     if (err) {
@@ -50,4 +52,168 @@ router.post('/query_obsnum', function(req, res, next)
 
 });
 
+router.post('/query_full', function(req, res, next) 
+{
+  //console.log('are they printed? > < !');
+  var query = req.body.queryModel,
+      checkbox = req.body.checkboxModel;
+
+  var RA = query.RA,
+      DEC = query.DEC,
+      RADIUS = query.RADIUS,
+      MJD_min = query.MJD_min,
+      MJD_max = query.MJD_max,
+      EXPTIME_min = query.EXPTIME_min,
+      EXPTIME_max = query.EXPTIME_max,
+      AZI_min = query.AZI_min,
+      AZI_max = query.AZI_max,
+      ALT_min = query.ALT_min,
+      ALT_max = query.ALT_max,
+      SEEING_min = query.SEEING_min,
+      SEEING_max = query.SEEING_max,
+      AIRMASS_min = query.AIRMASS_min,
+      AIRMASS_max = query.AIRMASS_max,
+      TAGID = query.TAGID,
+//      USERID = query.USERID,
+      PROPID = query.PROPID,
+      GROUPID = query.GROUPID,
+      OBSID = query.OBSID,
+      NLIMIT = query.LIMIT;
+
+  var pos_condition = '',
+      mjd_condition = '',
+      exptime_condition = '',
+      azimuth_condition = '',
+      altitude_condition = '',
+      seeing_condition = '',
+      airmass_condition = '',
+      tagid_condition = '',
+//      userid_condition = '',
+      propid_condition = '',
+      groupid_condition = '',
+      obsid_condition = '';
+
+  // If query position
+  if (checkbox.position) {
+    pos_condition = '     scircle(spoint(RADIANS(' + RA + '), RADIANS(' + DEC + ')), RADIANS(' + RADIUS + '/60.))~coords ';
+  };
+
+  // If query mjd
+  if (checkbox.mjd) {
+    mjd_condition = ' AND mjd BETWEEN ' + MJD_min + ' AND ' + MJD_max + ' ';
+  };
+
+  // If query exposure time
+  if (checkbox.exptime) {
+    exptime_condition = ' AND exptime BETWEEN ' + EXPTIME_min + ' AND ' + EXPTIME_max + ' ';
+  };
+
+  // If query azimuth
+  if (checkbox.azimuth) {
+    azimuth_condition = ' AND azimuth BETWEEN ' + AZI_min + ' AND ' + AZI_max + ' ';
+  };
+
+  // If query altitude
+  if (checkbox.altitutde) {
+    altitude_condition = ' AND altitude BETWEEN ' + ALT_min + ' AND ' + ALT_max + ' ';
+  };
+
+  // If query seeing
+  if (checkbox.seeing) {
+    seeing_condition = ' AND seeing BETWEEN ' + SEEING_min + ' AND ' + SEEING_max + ' ';
+  };
+
+  // If query airmass
+  if (checkbox.airmass) {
+    airmass_condition = ' AND \"AIRMASS\" BETWEEN ' + AIRMASS_min + ' AND ' + AIRMASS_max + ' ';
+  };
+
+  // If query tagid
+  if (checkbox.tagid) {
+    tagid_condition = ' AND \"TAGID_tsvector\" @@ to_tsquery(\'%' + TAGID + '%\') ';
+  };
+
+  // If query userid
+//  if (checkbox.userid) {
+//    userid_condition = ' AND \"USERID\" =\'' + USERID + '\' ';
+//  };
+
+  // If query propid
+  if (checkbox.propid) {
+    propid_condition = ' AND \"PROPID_tsvector\" @@ to_tsquery(\'%' + PROPID + '%\') ';
+  };
+
+  // If query groupgid
+  if (checkbox.groupid) {
+    groupid_condition = ' AND \"GROUPID\" =\'' + GROUPID + '\' ';
+  };
+
+  // If query obsid
+  if (checkbox.obsid) {
+    obsid_condition = ' AND \"OBSID\" =\'' + OBSID + '\' ';
+  };
+
+  var items = ' __obsnum, date, ra_degree, dec_degree, mjd, exptime, azimuth, altitude, \"AIRMASS\", seeing, \"TAGID\", \"PROPID\", \"GROUPID\", \"OBSID\" '
+
+  var condition = pos_condition + mjd_condition + exptime_condition + azimuth_condition + altitude_condition + seeing_condition + airmass_condition + tagid_condition + propid_condition + groupid_condition + obsid_condition;
+
+  condition = condition.slice(5,-1);
+  
+  var query_string = 'SELECT ' + items + ' FROM allkeys_testing_pgsphere_gin_matched_es WHERE ' + condition + ' LIMIT ' + NLIMIT
+
+  console.log(query_string);
+
+  pool.query(query_string, (err, result) => {
+    if (err) {
+      return console.error('Error executing query', err.stack)
+    }
+    //console.log(result);
+    res.send(result);
+    //res.render('table.ejs', { blabla: result });
+  })
+
+});
+
+
+router.post('/get_files', function(req, res, next) 
+{
+  //console.log(req.body);
+  var selected_rows = req.body;
+  var query_string = 'SELECT __filename from allkeys WHERE __obsnum IN (' + selected_rows + ') ';
+  
+  pool.query(query_string, (err, result) => {
+    if (err) {
+      return console.log(err.stack)
+    }
+    var result_formatted = [];
+    //console.log(result.rows.length);
+    for (var i=0; i<result.rows.length; i++) {
+      //console.log(result.rows[i].__filename);
+      result_formatted.push(result.rows[i].__filename);
+    }
+    console.log(result_formatted);
+    // Need full path here
+    var filename = 'temp_files/fileList_' + Date.now() + '.txt';
+    var filepath = path.join('/home/dbuser/DataArchive/pg-express-angularjs/public', filename);
+    console.log(filename);
+
+    fs.writeFileSync(filepath, result_formatted, function(err) {
+      if(err) {
+        return console.log(err);
+      }
+      else {
+        console.log("File created.");
+      }
+    });
+
+    // Only send back public path here
+    res.send(filename);
+    console.log(filename);
+    
+  })
+
+});
+
+
 module.exports = router;
+
